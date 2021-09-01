bf5bd63d379de1e1e9f9366b78d5a5b800f1f722
DVeronezNunes <dveroneznunes@gmail.com>
2021-09-01 00:49:45 -0300

    Added .gitignore

---
+++ src/main/java/Refferal.java
@@ -0,0 +1,11 @@
+public class Refferal {
+
+    private Student student;
+
+    public Refferal(Student student, Student referredStudent) {
+        this.student = student;
+        this.referredStudent = referredStudent;
+    }
+
+    private Student referredStudent;
+}
